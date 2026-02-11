9bbf371bf128f15c37a800b173fef5dfc35e2c90
Федотов Андрей <fedotov20.andrey04@gmail.com>
2026-02-11 16:57:10 +0300

    add full implementation of deepClone func

--- source/deepClone.js
+++ source/deepClone.js
@@ -11,4 +11,34 @@
  * 
  * @returns {Object}
  */
-const deepClone = obj => structuredClone(obj);
+function deepClone(obj) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+
+    if (obj instanceof Date) {
+        return new Date(obj);
+    }
+
+    if (obj instanceof Set) {
+        return new Set(obj);
+    }
+
+    if (obj instanceof Map) {
+        return new Map(obj);
+    }
+
+    if (Array.isArray(obj)) {
+        return obj.map(item => deepClone(item));
+    }
+
+    const cloned = {};
+
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            cloned[key] = deepClone(obj[key]);
+        }
+    }
+
+    return cloned;
+}
